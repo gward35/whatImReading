fbb7c3f47e75539f4f4c018b21f3d9bdffced91d
Gretchen Ward <gretchen.ward@isl.co>
2019-12-01 15:02:18 -0800

    Add api call to weather api

--- src/index.js
+++ src/index.js
@@ -6,4 +6,15 @@ new Vue({
   components: {
     StyledTitle,
   },
+  mounted() {
+    fetch(
+      'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ab309905673fcd9e124e789edcc7ab70'
+    )
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+      })
+  },
 })
