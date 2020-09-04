31a910fc83a2b7fa079a8dafd6673bf2870d8111
gward35 <g.g.ward90@gmail.com>
2020-09-03 20:20:19 -0400

    add css for animation

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -6,6 +6,29 @@ const Card = styled.div`
   color: #000;
   border: 1px solid #5facbf;
   background-color: #c5e1e8;
+  transition: border-bottom-right-radius 1s, box-shadow 0.8s, transform 1s;
+  overflow: hidden;
+  &:after {
+    position: absolute;
+    bottom: 0px;
+    right: -20px;
+    width: 0px;
+    height: 0px;
+    content: "";
+    background: linear-gradient(145deg, #c5e1e8 0%, #a7d2dc 50%);
+    box-shadow: -2px 2px 6px 2px #5facbf;
+    transform: skew(8deg);
+    transition: width 1s, height 1s;
+  }
+  &:hover {
+    border-bottom-right-radius: 60px;
+    box-shadow: 5px 0 20px 0 #5facbf;
+    &:after {
+      width: 65px;
+      height: 45px;
+      border-top-left-radius: 1px;
+    }
+  }
 
   @media screen and (max-width: 768px) {
     flex: 1 1 50%;
--- src/index.js
+++ src/index.js
@@ -85,7 +85,6 @@ new Vue({
         let xmlNode = new DOMParser().parseFromString(res.data, "text/xml");
         let formatted = xmlToJson(xmlNode);
         this.data = formatted.GoodreadsResponse.reviews.review;
-        console.log(this.data);
       });
   },
 });
