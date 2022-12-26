fc01ac39c7d8066e52f25409dab352788714c258
gward35 <g.g.ward90@gmail.com>
2022-12-26 16:40:04 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
 import axios from "axios";
 import BookCard from "./components/BookCard";
 import {ThemeProvider} from 'vue3-styled-components'
